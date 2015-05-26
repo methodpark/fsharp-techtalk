acc5e3e4cbf3b70d9f090cdeee1013854e3736c4
Benjamin Klüglein <scheibenkaes@googlemail.com>
2015-05-26 16:46:44 +0200

    add Json.NET

--- DotNetIntegration.fsx
+++ DotNetIntegration.fsx
@@ -3,11 +3,19 @@
 open System
 Console.WriteLine "Hello World"
 
-open System.IO
+#r "/home/b6n/dev/FsharpTechTalk/packages/Newtonsoft.Json.6.0.8/lib/net40/Newtonsoft.Json.dll"
 
-(new DirectoryInfo(Environment.CurrentDirectory)).GetFiles()
-|> Seq.filter (fun l -> l.Length < 100L)
-|> Seq.map (fun f -> f.Name)
+type Thingy = {
+    Name : string;
+    Skill : int32
+}
+
+open Newtonsoft.Json
+
+let t = {Name = "Dat thing"; Skill = 99}
+
+JsonConvert.SerializeObject t
+
 
 // nicer printing with compiler support!
 printfn "Hello World %A" [1; 2; 3]
