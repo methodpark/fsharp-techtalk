101b86d525efa9f0d0d2553e815c07b5e233c02c
Benjamin Klüglein <benjamin.klueglein@methodpark.de>
2015-05-29 12:14:58 +0200

    fix dll path reference now using a relative path

--- DotNetIntegration.fsx
+++ DotNetIntegration.fsx
@@ -1,24 +1,23 @@
-﻿
-// Easy use of the core .Net lib
+﻿// Easy use of the core .Net lib
 open System
+
 Console.WriteLine "Hello World" // <-- See?? No stupid ; and no ()
 
 // Easy use of your beloved .NET libraries
-#r "/home/b6n/dev/FsharpTechTalk/packages/Newtonsoft.Json.6.0.8/lib/net40/Newtonsoft.Json.dll"
+#r "packages/Newtonsoft.Json.6.0.8/lib/net40/Newtonsoft.Json.dll"
 
 open Newtonsoft.Json
 
-type Thingy = {
-    Name : string;
-    Skill : int32
-}
+type Thingy = 
+    { Name : string
+      Skill : int32 }
 
-let t = {Name = "Dat thing"; Skill = 99}
+let t = 
+    { Name = "Dat thing"
+      Skill = 99 }
 
 let s = JsonConvert.SerializeObject t
 
-
 // nicer printing with compiler support!
-printfn "Hello World %A" [1; 2; 3]
-
-// printfn "Hello World %d" "1 2 3"
+printfn "Hello World %A" [ 1; 2; 3 ]
+//printfn "Hello World %d" "1 2 3"
