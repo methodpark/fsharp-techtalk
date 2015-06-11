6bcf2b265b261563272f16f3bc65664ef9d5f419
Benjamin Klüglein <benjamin.klueglein@methodpark.de>
2015-06-11 17:15:20 +0200

    add type provider and unit of measure

---
+++ OcerNiceStuff.fsx
@@ -0,0 +1,51 @@
+﻿
+// unit of measure
+// https://msdn.microsoft.com/en-us/library/dd233243.aspx
+
+[<Measure>]
+type cm 
+
+[<Measure>]
+type m
+
+let square (l: float<m>) = l * 2.
+
+square 34.<m>
+
+[<Measure>]
+type euro
+
+[<Measure>]
+type cent
+
+
+// type provider
+
+#r "packages/FSharp.Data.2.2.2/lib/net40/FSharp.Data.dll"
+
+open FSharp.Data
+open System.Linq
+
+type Person = JsonProvider<"resources/schema.json">
+
+let s = "{ \"Name\": \"Paul\", \"Age\": 16 }"
+let p = Person.Parse s
+
+p.Age + 10
+
+
+let wb = WorldBankData.GetDataContext()
+
+let germany = wb.Countries.Germany
+
+wb.Countries.Germany.Indicators.``Adjusted savings: carbon dioxide damage (% of GNI)``.Values
+wb.Countries.China.Indicators.``Adjusted savings: carbon dioxide damage (% of GNI)``.Values                                
+
+                                
+germany.Indicators.``Cause of death, by communicable diseases and maternal, prenatal and nutrition conditions (% of total)``.Values
+wb.Countries.Chad.Indicators.``Cause of death, by communicable diseases and maternal, prenatal and nutrition conditions (% of total)``.Values
+
+// Kurzes Quiz der Hauptstädte
+[wb.Countries.Albania; wb.Countries.Lithuania; wb.Countries.Haiti]
+|> List.map (fun c -> c.CapitalCity)
+
--- PowerfulTypeSystem.fsx
+++ PowerfulTypeSystem.fsx
@@ -1,7 +1,28 @@
-﻿
-type User = {
-    Email : string;
-    Age : int32
-}
+﻿type User = 
+    { Email : string
+      Age : int32 }
 
-let u = {Email = "foo@bar"; Age = -1}
+let u = 
+    { Email = "foo@bar"
+      Age = -1 }
+
+type MailAddress = 
+    | MailAddress of string
+
+let createMail (s : string) = 
+    if System.Text.RegularExpressions.Regex.IsMatch(s, @"^\S+@\S+\.\S+$") then Some(MailAddress s)
+    else None
+
+type SafeUser = 
+    { Email : MailAddress
+      Age : int32 }
+
+let createUser (email, age) = 
+    let mail = createMail email
+    match mail with
+    | Some m -> 
+        Some { Email = m;
+               Age = age }
+    | None -> None
+
+createUser("mail", 12)
