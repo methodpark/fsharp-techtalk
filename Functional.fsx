619e6ff8c55f0f47165b14dc8d6abd5d803f2042
Benjamin Klüglein <scheibenkaes@googlemail.com>
2015-06-14 15:35:55 +0200

    add FizzBuzz example

--- Functional.fsx
+++ Functional.fsx
@@ -1,17 +1,17 @@
-﻿
-type Person = {
-    Name: string;
-    Age: int
-}
+﻿type Person = 
+    { Name : string
+      Age : int }
 
 // No fuzz initialization
-let m = {Name = "John"; Age = 67}
+let m = 
+    { Name = "John"
+      Age = 67 }
 
-// Free (like in free beer) equality!!1!elf
-
-(m = {Name = "John"; Age = 67})
-
-(m = {Name = "Jane"; Age = 67})
+(// Free (like in free beer) equality!!1!elf
+ m = { Name = "John"
+       Age = 67 })
+(m = { Name = "Jane"
+       Age = 67 })
 
 open System
 open System.IO
@@ -20,13 +20,9 @@ open System.IO
 |> Seq.filter (fun l -> l.Length < 100L)
 |> Seq.map (fun f -> f.Name)
 
-
 // Composing functions is just a breeze
-
-let upperCase (s: string) = s.ToUpperInvariant()
-
-let noSpaces (s: string) = s.Replace(" ", String.Empty)
-
+let upperCase (s : string) = s.ToUpperInvariant()
+let noSpaces (s : string) = s.Replace(" ", String.Empty)
 let f = upperCase >> noSpaces
 
 f "Nathan Drake"
@@ -38,8 +34,19 @@ type FB =
     | FizzBuzz
     | Number of int
 
-HERE
-let fb n =
+let (|MultOf3|_|) n = 
+    if n % 3 = 0 then Some MultOf3
+    else None
+
+let (|MultOf5|_|) n = 
+    if n % 5 = 0 then Some MultOf5
+    else None
+
+let fb n = 
     match n with
-    | _ when n / 3 = 0 -> Fizz
+    | MultOf3 & MultOf5 -> FizzBuzz
+    | MultOf3 -> Fizz
+    | MultOf5 -> Buzz
     | _ -> Number n
+
+[ 1..100 ] |> List.map fb
