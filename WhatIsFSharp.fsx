2770c255cfd7b681c9820a5a03fe5d76f558fa44
Benjamin Klüglein <benjamin.klueglein@methodpark.de>
2015-06-29 17:18:03 +0200

    add factorial example

--- WhatIsFSharp.fsx
+++ WhatIsFSharp.fsx
@@ -6,8 +6,16 @@ What is F#?
 
 -- F# Foundation http://fsharp.org/
 
-Basically it's a ML, closely related to OCaml, designed by Microsoft (Research)
+Basically it's a ML, closely related to OCaml, for the .NET runtime designed by Microsoft (Research).
 
 *)
 
-module WhatIsFSharp
+let someValue = 42L
+
+let someFunction (p1: string) = p1.ToUpper()
+
+let rec fac n = 
+    match n with
+    | 0 -> 1
+    | _ -> n * fac (n - 1)
+
